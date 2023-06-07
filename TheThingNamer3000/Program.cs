5242e5ac84c09e063b73e9eca226f152ee577c71
Gyllenhaal <86900724+GyllenhaalSP@users.noreply.github.com>
2023-06-07 04:13:55 +0200

    Initial commit.

---
+++ TheThingNamer3000/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("What kind of thing are we talking about?");
+string a = Console.ReadLine(); /* Reads the terminal input */
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+string b = Console.ReadLine();
+string c = "of Doom"; 
+string d = "3000"; //It stores the number as a string
+Console.WriteLine("The " + a + " " + b + " " + c + " " + d + "!"); //The error was the extra 'of' in the string
